8476dceefbdcc5cfc55b874e0edf076d39d110cb
mouheb <mouhebmh@gmail.com>
2021-03-15 22:24:43 +0100

    upload website

---
+++ index.php
@@ -0,0 +1,505 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Mouheb</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Mouheb CV By mouheb.com" />
+	<meta name="keywords" content="Mouheb CV By mouheb.com" />
+	<meta name="author" content="http://www.mouheb.com" />
+	
+	<link href="http://fonts.cdnfonts.com/css/julietta-messie" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Great Vibes' rel='stylesheet'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content="http://www.mouheb.com"/>
+	<meta property="og:site_name" content="Mouheb"/>
+	<meta property="og:description" content="CV Mhamdi Mouheb"/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+	<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class='spinner-wrapper'>
+		<div class="lds-ripple"><div></div><div></div></div>
+	</div>
+	
+	<div id="page">	
+	<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" [href]="/" style="background-image:url(images/cover_bg_3.jpg); cursor: pointer;" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<div class="display-t js-fullheight">
+						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+							<div class="profile-thumb rounded " style="background: url(images/mouheb.png); opacity: 0.8; border: 0.22cm white solid;"></div>
+							<h1>Mouheb Mhamdi</h1>
+							
+							<div class="" ><h3><span >Full Stack JAVA Developer </span></h3></div>
+							<p>
+								<ul class="fh5co-social-icons">
+									<li><a href="https://www.facebook.com/mouhebmhh"><i class="icon-facebook2"></i></a></li>
+									<li><a href="https://www.linkedin.com/in/mouhebmhamdi/"><i class="icon-linkedin2"></i></a></li>
+									<li><a href="http://www.mouheb.com"><i class="icon-dribbble2"></i></a></li>
+								</ul>
+							</p>
+							<nav class="navbar  p-t-10 p-l-10 p-r-10 p-b-10">
+								<div class="container-fluid">
+								  
+								  <ul class="nav navbar-nav">
+									
+									<li><a class="nav-link" href="#about">About Me</a></li>
+									<li><a class="nav-link" href="#resume ">My Resume</a></li>
+									<li><a class="nav-link" href="#service">My Servecies</a></li>
+									<li><a class="nav-link" href="#skills">Skills</a></li>
+									<li><a class="nav-link" href="#hire">Hire Me</a></li>
+									<li><a class="nav-link" href="#contact">Contact Me</a></li>
+								  </ul>
+								</div>
+							  </nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="about" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>About Me</h2>
+				</div>
+			</div>
+			<div class="row ">
+				<div class="col-md-5">
+					<ul class="info">
+						<li><span class="first-block">Full Name:</span><span class="second-block">Mouheb Mhamdi</span></li>
+						<li><span class="first-block">Phone:</span><span class="second-block">+216 50 566 033</span></li>
+						<li><span class="first-block">Email:</span><span class="second-block">infos@mouheb.com	</span></li>
+						<li><span class="first-block">Website:</span><a class="second-block" href="http://www.mouheb.com">www.mouheb.com</a></li>
+						<li class="p-l-100"><span class="first-block">Address:</span><span class="second-block">3130 Monji Slim 94th Street, Kairouan Tunisia   </span></li>
+					</ul>
+				</div>
+				<div class="col-md-7">
+					
+					<p>
+						 I am a JAVA full stack developer, I master a different languages and frameworks among which spring boot,
+						 hibernate, JAVA, android,Java swing, SQL,noSQL,plSQL in addition, I excelled in the field of graphic 
+						 design (photoshop, illustrator).<br> 
+					
+						 What characterizes me is creativity, I put all my skills in the field of development.<br> 
+					
+						This creativity, coupled with a great autonomy, will allow me to excel tomorrow in several positions. 
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							
+							<li><a href="https://www.facebook.com/mouhebmhh"><i class="icon-facebook3"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/mouhebmhamdi/"><i class="icon-linkedin2"></i></a></li>
+							<li><a href="http://www.mouheb.com"><i class="icon-dribbble2"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="resume" class="fh5co-bg-color">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>My Resume</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0">
+					<ul class="timeline">
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Formation</h3></div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Senior Developer</h3>
+									<span class="company">Company Name - 2016 - Current</span>
+								</div>
+								<div class="timeline-body">
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Junior Developer</h3>
+									<span class="company">Company Name - 2013 - 2015</span>
+								</div>
+								<div class="timeline-body">
+									<p>Far far away, behind the word mountains, they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">UI/UX Designer</h3>
+									<span class="company">Company Name - 2010 - 2012</span>
+								</div>
+								<div class="timeline-body">
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div>
+						</li>
+
+						<br>
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Education</h3></div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Masters Degree</h3>
+									<span class="company">higher institute of computer science mahdia - <br> 2020 - Now</span>
+								</div>
+								<div class="timeline-body">
+									<p>Data scientiste student </p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Licence Degree</h3>
+									<span class="company">higher institute of computer science mahdia - <br> - 2018 - 2020</span>
+								</div>
+								<div class="timeline-body">
+									<p>Basic Bachelor in Computer Science </p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Bachelors Degree</h3>
+									<span class="company">secondary school of Kairouan<br> - 2017 - 2018</span>
+								</div>
+								<div class="timeline-body">
+									<p>Bachelorettes of computer science </p>
+								</div>
+							</div>
+						</li>
+						
+			    	</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="fh5co-features" class="animate-box">
+		<div id="service" class="container">
+			<div class="services-padding">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<h2>My Services</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="fa fa-desktop"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Web Design</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="fa fa-laptop"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Web development</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-search"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Analytics</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-4 text-center">
+
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-bargraph"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Web Developent</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-md-4 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-genius"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Web Marketing</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-4 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-chat"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Support</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-skills" class="animate-box">
+		<div id="skills" class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Skills</h2>
+				</div>
+			</div>
+			<div class="row row-pb-md">
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="97"><span><strong>HTML5</strong>97%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="93"><span><strong>Bootstrap</strong>93%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>css3</strong>90%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="50"><span><strong>jQuery</strong>50%</span></div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="89"><span><strong>PHP</strong>89%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>SQL</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="65"><span><strong>Hadoop</strong>65%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="75"><span><strong>NoSQL</strong>75%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>Spring Boot</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="75"><span><strong>Hibernate</strong>75%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>Thymleaf</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>JAVA</strong>90%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>J2EE</strong>90%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="35"><span><strong>Angular</strong>35%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div   class="chart" data-percent="70"><span><strong>Programmation SAS</strong>70%</span></div>
+				</div>
+				
+			</div>
+			
+		</div>
+		<div id="hire"></div>
+	</div>
+
+
+
+	
+	<div id="fh5co-started" class="fh5co-bg-dark">
+		<div class="overlay"></div>
+		<div  class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Hire Me!</h2>
+					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+					<p><a href="#" class="btn btn-default btn-lg " >Contact Me</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-consult">
+		<div class="video fh5co-video " style="background-image: url(images/cover_bg_1.jpg);">
+			<div class="overlay "></div>
+		</div>
+		<div class="choose animate-box">
+			<h2 id="contact">Contact</h2>
+			<?php 
+                            $Msg = "";
+                            if(isset($_GET['error']))
+                            {
+                                $Msg = " Please Fill in the Blanks ";
+                                echo '<div class="alert alert-danger">'.$Msg.'</div>';
+                            }
+
+                            if(isset($_GET['success']))
+                            {
+                                $Msg = " Your Message Has Been Sent ";
+                                echo '<div class="alert alert-success">'.$Msg.'</div>';
+                            }
+                        
+                        ?>
+			<form action="#">
+				<div class="row form-group">
+					<div class="col-md-6">
+						<input type="text" id="fname" name="UName" class="form-control" placeholder="Your full name" required>
+					</div>
+				</div>
+				
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="email" name="Email" id="email" class="form-control" placeholder="Your email address">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="text" name="Subject" id="subject" class="form-control" placeholder="Your subject of this message">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<textarea name="msg" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+					</div>
+					
+				</div>
+				<div class="row from-group">
+					<div class="col-md-12">
+					
+					</div>
+				</div>
+				<div class="form-group">
+					<input type="submit" value="Send Message" class="btn btn-primary">
+				</div>
+
+			</form>	
+		</div>
+	</div>
+
+
+	
+	<div id="fh5co-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>&copy; 2021 . All Rights Reserved. <br>Designed by <a href="http://mouheb.com" >Mouheb</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="gototop js-top" >
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+	<script>
+           let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+              window.addEventListener('load', function () {
+              // spinnerWrapper.style.display = 'none';
+              spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+           });
+	</script>
+	<!-- jQuery -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.js"></script> 
+	
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src="js/jquery.easypiechart.min.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
+	
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
+	
